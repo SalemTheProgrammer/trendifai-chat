3efd940bb895cb113d97ffd11d5106bc86d7ee9f
Salem Dahmani <salem.dahmani@esprit.tn>
2025-01-14 22:01:56 +0100

    lol

--- next.config.js
+++ next.config.js
@@ -1,36 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['your-domain.com'],
-    unoptimized: false,
-  },
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ['framer-motion', '@heroicons/react'],
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  webpack: (config, { dev, isServer }) => {
-    // Optimize bundle size
-    config.optimization = {
-      ...config.optimization,
-      splitChunks: {
-        chunks: 'all',
-        minSize: 20000,
-        maxSize: 70000,
-        cacheGroups: {
-          commons: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all',
-          },
-        },
-      },
-    };
-    return config;
-  },
+  swcMinify: true,
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
